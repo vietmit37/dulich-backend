533a30d9145061127132388cee95bedd793e521d
ndk2810 <khangdeptrai2810@gmail.com>
2021-06-09 23:34:03 +0700

    thanhtoan

--- routes/datTour.js
+++ routes/datTour.js
@@ -54,7 +54,7 @@ router.route('/').post((req, res) => {
         });
 })
 
-//GET thông tin Đặt tour
+//GET tất cả các records trong table DATTOUR
 router.route('/getDatTour').get((req, res) => {
 
     const query = `SELECT * FROM DATTOUR`
@@ -123,5 +123,56 @@ router.route('/chiTiet/:IDDat').get((req, res) => {
             conn.close();
         });
 })
+
+//GET thông tin Đặt tour chi tiết bằng các trường thông tin
+router.route('/chiTiet/useThongTinCT').post((req, res) => {
+    const hoTen = req.body.HoTen
+    const soDienThoai = req.body.SoDienThoai
+    const ngayDi = req.body.NgayDi
+    const IDTour = req.body.IDTour
+
+    const query = `SELECT * 
+    FROM DATTOUR 
+    WHERE HoTen = N'${hoTen}' AND SoDienThoai = '${soDienThoai}' AND IDTour = ${IDTour} AND NgayDi = '${ngayDi}';`
+
+    // Create connection instance
+    const conn = new sql.ConnectionPool(dbConfig);
+
+    conn.connect()
+        // Successfull connection
+        .then(function () {
+
+            // Create request instance, passing in connection instance
+            const req = new sql.Request(conn);
+
+            // Call mssql's query method passing in params
+            req.query(query)
+                .then(function (recordsets) {
+                    res.send(recordsets.recordset);
+                    conn.close();
+                })
+                // Handle sql statement execution errors
+                .catch(function (err) {
+                    console.log(err);
+                    conn.close();
+                })
+
+        })
+        // Handle connection errors
+        .catch(function (err) {
+            console.log(err);
+            conn.close();
+        });
+})
+
+// //Thanh toán Stripe
+// router.route('/thanhToan').post(cors(), async (req, res) => {
+//     let { amount, id } = req.body
+//     try {
+//         const payment = await stripe
+//     } catch (error) {
+
+//     }
+// })
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors')
+const stripe = require("stripe")("sk_test_51IzjQtAHeeyUAi3RSHbt1sPob40b8icOzAMQYELld6hLGtJvMS1oqZtcvx0vcetqLSQMrgLS8nPeurvLJVMI35Mb00vo0mXV7B")
 
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
-
 
 //IMPORT ROUTERS
 const diaDiemRouter = require('./routes/diaDiem')
@@ -17,12 +17,37 @@ const theLoaiTourRouter = require('./routes/theLoaiTour')
 const khungThoiGianRouter = require('./routes/khungThoiGian')
 const datTourRouter = require('./routes/datTour')
 
+app.post('/thanhToan', cors(), async (req, res) => {
+    let { amount, id, description } = req.body
+    try {
+        const payment = await stripe.paymentIntents.create({
+            amount,
+            currency: "VND",
+            description,
+            payment_method: id,
+            confirm: true
+        })
+        res.json({
+            message: "Payment successful on backend !",
+            success: true
+        })
+    } catch (error) {
+        console.log(error)
+        res.json({
+            message: "Payment failed on backend !",
+            success: false
+        })
+    }
+})
+
 app.use('/diaDiem', diaDiemRouter)
 app.use('/tour', tourRouter)
 app.use('/theLoaiTour', theLoaiTourRouter)
 app.use('/khungThoiGian', khungThoiGianRouter)
 app.use('/datTour', datTourRouter)
-
+app.use('/', (req, res) => {
+    res.send('Welcome to API tour du lịch OKA2')
+})
 app.listen(port, () => {
     console.log(`Backend đang chạy tại http://localhost:${port}`)
 })
