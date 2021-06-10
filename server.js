8ccf28d71c58bf2d7d196ab16546942db3dda7a9
ndk2810 <khangdeptrai2810@gmail.com>
2021-06-10 12:32:13 +0700

    deploy fv

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000
+const port = 5000
 const cors = require('cors')
 const stripe = require("stripe")("sk_test_51IzjQtAHeeyUAi3RSHbt1sPob40b8icOzAMQYELld6hLGtJvMS1oqZtcvx0vcetqLSQMrgLS8nPeurvLJVMI35Mb00vo0mXV7B")
 
